d507465ffdd1062cf7ec1a031b2957f228e59cd1
Gerard Castrelo Saperas <gcsaperas@konodrac.com>
2020-10-02 09:10:48 +0200

    initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { RegisterComponent } from './user/register/register.component';
+import { LoginComponent } from './user/login/login.component';
+import { LandingComponent } from './layout/landing/landing.component';
+import { RoutinesComponent } from './routines/routines.component';
+import { MovementsComponent } from './movements/movements.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    RegisterComponent,
+    LoginComponent,
+    LandingComponent,
+    RoutinesComponent,
+    MovementsComponent
   ],
   imports: [
     BrowserModule
